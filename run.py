370a8b955c467d213b9ba3540a4527856ceac267
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-06-11 21:10:57 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def greet(name):
+    print("Hey there!")
+    print("welcome, " + name)
+
+
+
+
+greet("Sonu")
+
+def say_heloo(sam):
+    print("Hello!, " + sam)
+    print("Welcome to the coding school")
+
+sam = "Kulwinder Singh"
+say_heloo(sam)
